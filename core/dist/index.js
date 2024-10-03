72298e2f5831acc6e8ad56a6458140164494255a
smosco <go1ruf2tk3@gmail.com>
2024-10-04 01:22:38 +0900

    feat: demo에서 예시 버튼 컴포넌트 사용을 위해 core 빌드

---
+++ core/dist/index.js
@@ -0,0 +1,9 @@
+// src/components/Button.tsx
+import { jsx } from "react/jsx-runtime";
+function Button({ label, onClick }) {
+  return /* @__PURE__ */ jsx("button", { onClick, children: label });
+}
+export {
+  Button
+};
+//# sourceMappingURL=index.js.map
