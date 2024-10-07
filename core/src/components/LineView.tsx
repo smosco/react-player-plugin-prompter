2055e095b813f47efe68589338a8ca60e6c6833a
smosco <go1ruf2tk3@gmail.com>
2024-10-07 12:20:11 +0900

    feat: 한줄씩 보기 모드에서 재생 시간에 맞는 자막 표시

--- core/src/components/LineView.tsx
+++ core/src/components/LineView.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { LanguageCode, Subtitle } from '../interfaces/Scripts';
-import { useState } from 'react';
+// import { useState } from 'react';
 import arrow_back from '../assets/icons/arrow_back.svg';
 import arrow_forward from '../assets/icons/arrow_forward.svg';
 import { TextDisplay } from './TextDisplay';
@@ -9,6 +9,7 @@ import styles from './ReactScriptPlayer.module.scss';
 interface LineViewProps {
   subtitles: Subtitle[];
   selectedLanguages: LanguageCode[];
+  currentSubtitleIndex: number;
   seekTo: (timeInSeconds: number) => void;
   onSelectWord: (word: string, subtitle: Subtitle, index: number) => void;
 }
@@ -16,23 +17,24 @@ interface LineViewProps {
 export function LineView({
   subtitles,
   selectedLanguages,
+  currentSubtitleIndex,
   seekTo,
   onSelectWord,
 }: LineViewProps) {
-  const [currentSubtitleIndex, setCurrentSubtitleIndex] = useState(0);
+  // const [currentSubtitleIndex, setCurrentSubtitleIndex] = useState(0);
   const totalSubtitles = subtitles.length;
 
   const handlePrevious = () => {
     if (currentSubtitleIndex > 0) {
       seekTo(subtitles[currentSubtitleIndex - 1].startTimeInSecond);
-      setCurrentSubtitleIndex((prev) => prev - 1);
+      // setCurrentSubtitleIndex((prev) => prev - 1);
     }
   };
 
   const handleNext = () => {
     if (currentSubtitleIndex < totalSubtitles - 1) {
       seekTo(subtitles[currentSubtitleIndex + 1].startTimeInSecond);
-      setCurrentSubtitleIndex((prev) => prev + 1);
+      // setCurrentSubtitleIndex((prev) => prev + 1);
     }
   };
 
@@ -46,13 +48,15 @@ export function LineView({
           <img src={arrow_forward} alt="Forward Arrow" />
         </button>
       </div>
-
       {/* TextDisplay에서 현재 자막과 선택된 언어를 표시 */}
-      <TextDisplay
-        subtitle={subtitles[currentSubtitleIndex]}
-        selectedLanguages={selectedLanguages}
-        onSelectWord={onSelectWord}
-      />
+      {subtitles[currentSubtitleIndex] && (
+        // TODO:자막 프로그레스바
+        <TextDisplay
+          subtitle={subtitles[currentSubtitleIndex]}
+          selectedLanguages={selectedLanguages}
+          onSelectWord={onSelectWord}
+        />
+      )}
     </div>
   );
 }
