041cc037f5907687afcbc829eff3fc18183802aa
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-11-11 12:18:46 +0900

    chore : dev 후 실행 파일

--- core/dist/utils/findClickedIndex.d.ts
+++
@@ -1,3 +0,0 @@
-import { Script } from 'interfaces/Scripts';
-export declare const findClickedIndex: (scripts: Script[] | undefined, clickedIndex: number) => number | null;
-//# sourceMappingURL=findClickedIndex.d.ts.map
