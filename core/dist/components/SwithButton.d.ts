019ec20fc90e70bb76bd434c00725c19f123d91a
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-11-12 12:15:38 +0900

    chore : 변경된 코드 반영된 제대로된 dist파일

--- core/dist/components/SwithButton.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-export interface SwitchButtonProps {
-    toggle: React.Dispatch<React.SetStateAction<boolean>>;
-    isToggle: boolean;
-}
-export declare function SwitchButton({ toggle, isToggle, }: SwitchButtonProps): JSX.Element;
-//# sourceMappingURL=SwithButton.d.ts.map
