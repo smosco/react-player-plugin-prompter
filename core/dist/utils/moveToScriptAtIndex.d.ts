5f53649387fde3adb8936821094f886fb38d7396
Han Hyun <62870362+smosco@users.noreply.github.com>
2024-12-17 22:58:55 +0900

    remove : core/dist from git (#51)
    * remove: core/dist and ignore it in Git
    
    * feat: version up to 1.0.2
    
    * chore: add command to build core before demo during vercel deployment

--- core/dist/utils/moveToScriptAtIndex.d.ts
+++
@@ -1,3 +0,0 @@
-import { Script } from '../interfaces/Scripts';
-export declare const moveToScriptAtIndex: <T extends string>(index: number, scripts: Script<T>[], seekTo: (timeInSeconds: number) => void) => void;
-//# sourceMappingURL=moveToScriptAtIndex.d.ts.map
